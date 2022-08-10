c35b941f19be17fb371df63f442e2cb01cdaa403
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-08-10 10:50:07 -0600

    commit tree 86f96fde8f3ec084cd349806bcd06a116bc50d7e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'plant/index'
+  get 'plant/show'
+  get 'plant/create'
+  resources :plants, only: [:index, :show, :create]
+  
 end
